95468c4a9a18148f9c74781324709ac7eb0df93d
Perry Du <i@perry.asia>
2013-05-11 22:09:44 +0800

    4th continuous building. Add index and archive for the static blog.

--- hazel/build.py
+++ hazel/build.py
@@ -5,9 +5,11 @@ import re
 import time
 import yaml
 import codecs
+import shutil
 import datetime
 import markdown
 
+from operator import itemgetter
 from hazel.utils import ObjectDict
 from clint.textui import puts, indent, colored
 from jinja2 import Template, Environment, FileSystemLoader
@@ -18,11 +20,6 @@ class Post(ObjectDict):
         self.date = []
 
 
-def load_yaml():
-    with open('config.yaml', 'r') as f:
-        g.config = yaml.load(f.read())
-
-
 def path_to_file(directory, filename):
     return os.path.join(g.config['path'], directory, filename)
 
@@ -31,19 +28,31 @@ def path_to_directory(directory):
     return os.path.join(g.config['path'], directory)
 
 
-def path_to_template():
-    return os.path.join(g.config['path'], 'templates', g.config['template'])
+def path_to_template_assets():
+    return os.path.join(g.config['path'], 'templates', g.config['template'], 'assets')
 
 
-def render_template(template, **context):
-    env = Environment(loader=FileSystemLoader(path_to_template()))
+def render_template(template, **content):
+    env = Environment(loader=FileSystemLoader(g.template))
     template = env.get_template(template)
-    return template.render(context)
+    return template.render(content)
 
 
 def write(filename, content):
     with codecs.open(path_to_file('site', filename + '.html'), mode='w', encoding='utf-8') as f:
         f.write(content)
+
+
+def load_yaml():
+    with open('config.yaml', 'r') as f:
+        g.config = yaml.load(f.read())
+
+
+def load_path():
+    g.template = os.path.join(g.config['path'], 'templates', g.config['template'])
+    g.template_assets = os.path.join(g.template, 'assets')
+    g.site = path_to_directory('site')
+    g.site_assets = os.path.join(g.site, 'assets')
 
 
 def read_post(filename):
@@ -57,21 +66,12 @@ def read_post(filename):
                 meta = yaml.load(''.join(lines[:l]))
                 post.title = meta['title']
                 post.slug = re.split('\.+', filename)[0]
-                try:
-                    post.date = meta['date'].strftime('%B %d, %Y')
-                except:
-                    date = meta['date'].split('-')
-                    post.date = datetime.date(int(date[0]), int(date[1]), int(date[2])).strftime('%B %d, %Y')
-                    puts(colored.red('Date is not exact proper in file %s, but it\'s ok.' % filename))
+                post.date = meta['date'].strftime('%Y-%m-%d')
                 post.content = markdown.markdown(''.join(lines[l + 1:]))
-                post.html = render_template('post.html', title=post.title, slug=post.slug, date=post.date, content=post.content)
-                if len(g.posts) <= g.config['post_per_page']:
-                    g.posts.append(post)
+                g.posts.append(post)
                 break
-    try:
-        write(re.split('\.+', filename)[0], post.html)
-    except:
-        pass
+    html = render_template('post.html', title=post.title, slug=post.slug, date=post.date, content=post.content)
+    write(re.split('\.+', filename)[0], html)
 
 
 def read_posts():
@@ -85,20 +85,33 @@ def read_posts():
                 puts('File %s is read successfully.' % p)
             except:
                 puts(colored.yellow('File %s is not concord with the style of hazel document.' % p))
+        g.posts = sorted(g.posts, key=itemgetter('date'), reverse=True) 
     else:
-        puts(colored.red('No files in the post directory.'))
+        puts(colored.red('No posts in the posts directory.'))
 
 
 def build_index():
-    html = render_template('index.html', posts=g.posts)
+    html = render_template('index.html', posts=g.posts[:g.config['index_post']])
     write('index', html)
 
+
+def build_archive():
+    html = render_template('archive.html', posts=g.posts)
+    write('archive', html)
+
+def copy_assets():
+    if os.path.exists(g.site_assets):
+        shutil.rmtree(g.site_assets)
+    shutil.copytree(g.template_assets, g.site_assets)
 
 def build():
     g.time = time.time()
     load_yaml()
+    load_path()
     read_posts()
     build_index()
+    build_archive()
+    copy_assets()
     puts(colored.green('Well Done! Buidng process cost %.3fs in total.' % (time.time() - g.time)))
 
 g = ObjectDict
