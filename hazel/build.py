993c9ab14a0f7085edab9c1b3dd53394466a0f93
Perry Du <i@perry.asia>
2013-05-11 12:28:50 +0800

    Add post per page option in yaml.

--- hazel/build.py
+++ hazel/build.py
@@ -13,7 +13,7 @@ from clint.textui import puts, indent, colored
 from jinja2 import Template, Environment, FileSystemLoader
 
 
-class Post:
+class Post(ObjectDict):
     def __init__(self):
         self.date = []
 
@@ -35,6 +35,17 @@ def path_to_template():
     return os.path.join(g.config['path'], 'templates', g.config['template'])
 
 
+def render_template(template, **context):
+    env = Environment(loader=FileSystemLoader(path_to_template()))
+    template = env.get_template(template)
+    return template.render(context)
+
+
+def write(filename, content):
+    with codecs.open(path_to_file('site', filename + '.html'), mode='w', encoding='utf-8') as f:
+        f.write(content)
+
+
 def read_post(filename):
     post = Post()
     with codecs.open(path_to_file('posts', filename), mode='r', encoding='utf-8') as f:
@@ -43,47 +54,52 @@ def read_post(filename):
             lines.pop(0)
         for l, line in enumerate(lines):
             if line.startswith('---'):
-                post.yaml = yaml.load(''.join(lines[:l]))
-                post.title = post.yaml['title']
+                meta = yaml.load(''.join(lines[:l]))
+                post.title = meta['title']
+                post.slug = re.split('\.+', filename)[0]
                 try:
-                    post.date = post.yaml['date'].strftime('%B %d, %Y')
+                    post.date = meta['date'].strftime('%B %d, %Y')
                 except:
-                    date = post.yaml['date'].split('-')
+                    date = meta['date'].split('-')
                     post.date = datetime.date(int(date[0]), int(date[1]), int(date[2])).strftime('%B %d, %Y')
                     puts(colored.red('Date is not exact proper in file %s, but it\'s ok.' % filename))
                 post.content = markdown.markdown(''.join(lines[l + 1:]))
-                env = Environment(loader=FileSystemLoader(path_to_template()))
-                template = env.get_template('post.html')
-                post.html = template.render(title=post.title, date=post.date, content=post.content)
+                post.html = render_template('post.html', title=post.title, slug=post.slug, date=post.date, content=post.content)
+                if len(g.posts) <= g.config['post_per_page']:
+                    g.posts.append(post)
                 break
-        return post
+    try:
+        write(re.split('\.+', filename)[0], post.html)
+    except:
+        pass
 
 
 def read_posts():
+    g.posts = []
     pattern = re.compile('.+\.md$', re.I)
-    files = [f for f in os.listdir(path_to_directory('posts')) if pattern.match(f)]
-    if files:
-        for f in files:
-            post = read_post(f)
+    posts = [p for p in os.listdir(path_to_directory('posts')) if pattern.match(p)]
+    if posts:
+        for p in posts:
             try:
-                write(re.split('\.+', f)[0], post.html)
-                puts('File %s is read successfully.' % f)
+                read_post(p)
+                puts('File %s is read successfully.' % p)
             except:
-                puts(colored.yellow('File %s is not concord with the style of hazel document.' % f))
-        puts(colored.green('Well Done! Buidng process cost %.3fs in total.' % (time.time() - g.time)))
+                puts(colored.yellow('File %s is not concord with the style of hazel document.' % p))
     else:
         puts(colored.red('No files in the post directory.'))
 
 
-def write(filename, content):
-    with codecs.open(path_to_file('site', filename + '.html'), mode='w', encoding='utf-8') as f:
-        f.write(content)
+def build_index():
+    html = render_template('index.html', posts=g.posts)
+    write('index', html)
+
 
 def build():
     g.time = time.time()
     load_yaml()
     read_posts()
+    build_index()
+    puts(colored.green('Well Done! Buidng process cost %.3fs in total.' % (time.time() - g.time)))
 
-
-g = ObjectDict()
+g = ObjectDict
 build()
