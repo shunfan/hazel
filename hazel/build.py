ef4a2ec2144034a18ed61d166195da572aff8562
Perry Du <i@perry.asia>
2013-05-12 12:32:14 +0800

    5th continuos building. various updates.

--- hazel/build.py
+++ hazel/build.py
@@ -2,45 +2,20 @@
 
 import os
 import re
-import time
 import yaml
 import codecs
 import shutil
-import datetime
 import markdown
 
 from operator import itemgetter
-from hazel.utils import ObjectDict
+from datetime import datetime, date, time
 from clint.textui import puts, indent, colored
 from jinja2 import Template, Environment, FileSystemLoader
+from hazel.utils import ObjectDict, path_to_file, render_template, write, g
 
 
 class Post(ObjectDict):
-    def __init__(self):
-        self.date = []
-
-
-def path_to_file(directory, filename):
-    return os.path.join(g.config['path'], directory, filename)
-
-
-def path_to_directory(directory):
-    return os.path.join(g.config['path'], directory)
-
-
-def path_to_template_assets():
-    return os.path.join(g.config['path'], 'templates', g.config['template'], 'assets')
-
-
-def render_template(template, **content):
-    env = Environment(loader=FileSystemLoader(g.template))
-    template = env.get_template(template)
-    return template.render(content)
-
-
-def write(filename, content):
-    with codecs.open(path_to_file('site', filename + '.html'), mode='w', encoding='utf-8') as f:
-        f.write(content)
+    pass
 
 
 def load_yaml():
@@ -49,70 +24,93 @@ def load_yaml():
 
 
 def load_path():
+    g.posts = os.path.join(g.config['path'], 'posts')
     g.template = os.path.join(g.config['path'], 'templates', g.config['template'])
     g.template_assets = os.path.join(g.template, 'assets')
-    g.site = path_to_directory('site')
+    g.site = os.path.join(g.config['path'], 'site')
     g.site_assets = os.path.join(g.site, 'assets')
+    if os.path.exists(g.site):
+        shutil.rmtree(g.site)
+        os.mkdir(g.site)
+    else:
+        os.mkdir(g.site)
 
+
+def load_jinja():
+    g.template = Environment(loader=FileSystemLoader(g.template))
+    g.template.globals['site_name'] = g.config['site_name']
 
 def read_post(filename):
     post = Post()
-    with codecs.open(path_to_file('posts', filename), mode='r', encoding='utf-8') as f:
+    puts('Reading %s now...' % filename)
+    with codecs.open(path_to_file(g.posts, filename), mode='r', encoding='utf-8') as f:
         lines = f.readlines()
         if lines[0].startswith('---'):
             lines.pop(0)
         for l, line in enumerate(lines):
-            if line.startswith('---'):
+            if line.startswith('---') or line.startswith('\n'):
                 meta = yaml.load(''.join(lines[:l]))
+                meta = dict((a.lower(), b) for a,b in meta.iteritems())
                 post.title = meta['title']
                 post.slug = re.split('\.+', filename)[0]
-                post.date = meta['date'].strftime('%Y-%m-%d')
+                post.date = meta['date']
+                if type(post.date) is date:
+                    post.date = datetime.combine(post.date, time(0, 0))
                 post.content = markdown.markdown(''.join(lines[l + 1:]))
-                g.posts.append(post)
+                g.archive.append(post)
+                with indent(2, quote='>'):
+                    puts('read successfully.')
                 break
-    html = render_template('post.html', title=post.title, slug=post.slug, date=post.date, content=post.content)
-    write(re.split('\.+', filename)[0], html)
+    try:
+        html = render_template('post.html', post=post, config=g.config)
+        with indent(2, quote='>'):
+            puts('rendered successfully.')
+        try:
+            write(g.site, re.split('\.+', filename)[0], html)
+            with indent(2, quote='>'):
+                puts('written successfully.')
+        except:
+            puts(colored.red('%s could not be written.' % filename))
+    except:
+        puts(colored.red('%s could not be rendered.' % filename))
 
 
 def read_posts():
-    g.posts = []
+    g.archive = []
     pattern = re.compile('.+\.md$', re.I)
-    posts = [p for p in os.listdir(path_to_directory('posts')) if pattern.match(p)]
+    posts = [p for p in os.listdir(g.posts) if pattern.match(p)]
     if posts:
         for p in posts:
-            try:
-                read_post(p)
-                puts('File %s is read successfully.' % p)
-            except:
-                puts(colored.yellow('File %s is not concord with the style of hazel document.' % p))
-        g.posts = sorted(g.posts, key=itemgetter('date'), reverse=True) 
+            read_post(p)
+        try:
+            g.archive = sorted(g.archive, key=itemgetter('date'), reverse=True)
+        except:
+            puts(colored.red('Archive can not be sorted, please check whether the date of post is right.'))
     else:
         puts(colored.red('No posts in the posts directory.'))
 
 
 def build_index():
-    html = render_template('index.html', posts=g.posts[:g.config['index_post']])
-    write('index', html)
+    html = render_template('index.html', posts=g.archive[:g.config['index_post']], config=g.config)
+    write(g.site, 'index', html)
 
 
 def build_archive():
-    html = render_template('archive.html', posts=g.posts)
-    write('archive', html)
+    html = render_template('archive.html', posts=g.archive, config=g.config)
+    write(g.site, 'archive', html)
 
 def copy_assets():
-    if os.path.exists(g.site_assets):
-        shutil.rmtree(g.site_assets)
     shutil.copytree(g.template_assets, g.site_assets)
 
 def build():
-    g.time = time.time()
     load_yaml()
     load_path()
+    load_jinja()
     read_posts()
     build_index()
     build_archive()
     copy_assets()
-    puts(colored.green('Well Done! Buidng process cost %.3fs in total.' % (time.time() - g.time)))
+    puts(colored.green('Complete! Buidng process cost %.3fs in total.'))
 
-g = ObjectDict
+
 build()
--- hazel/utils.py
+++ hazel/utils.py
@@ -1,6 +1,11 @@
 # coding=utf-8
 
+import os
+import codecs
+
+
 class ObjectDict(dict):
+	# Copied from the source code of tornado
     def __getattr__(self, name):
         try:
             return self[name]
@@ -9,3 +14,19 @@ class ObjectDict(dict):
 
     def __setattr__(self, name, value):
         self[name] = value
+
+
+def path_to_file(directory, filename):
+    return os.path.join(directory, filename)
+
+
+def render_template(template, **content):
+    template = g.template.get_template(template)
+    return template.render(content)
+
+
+def write(directory, filename, content):
+    with codecs.open(path_to_file(directory, filename + '.html'), mode='w', encoding='utf-8') as f:
+        f.write(content)
+
+g = ObjectDict
