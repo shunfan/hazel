160ca8bfd0b5304cdf3f3261236e8951a134aed0
Perry Du <i@perry.asia>
2013-05-12 16:21:03 +0800

    Modify the development status to 1.

--- setup.py
+++ setup.py
@@ -2,29 +2,36 @@
 
 from setuptools import setup, find_packages
 
-import hazel
+from hazel import __version__
 
-setup(name='Hazel',
-      version='0.1',
-      description='Gorgeous Static Blog System',
-      author='Shunfan Du',
-      author_email='i@perry.asia',
-      url='https://github.com/shunfan/hazel',
-      packages=find_packages(),
-      include_package_data=True,
-      license='MIT License',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Environment :: Console',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: MacOS',
-          'Operating System :: POSIX',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.1',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
+setup(
+    name='hazel',
+    version=__version__,
+    author='Shunfan Du',
+    author_email='i@perry.asia',
+    description='Gorgeous Static Blog System',
+    license='MIT',
+    keywords='static blog markdown',
+    url='https://github.com/shunfan/hazel',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+      'console_scripts': [
+        'hazel = hazel.build:build'
       ],
-     )
+    },
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT Licenseh',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
