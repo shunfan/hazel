e6a5c7b64a52190104f7b649e3425a141357b761
Perry Du <i@perry.asia>
2013-05-13 21:17:09 +0800

    new function safe_mkdir, force_mkdir and create.

--- hazel/utils.py
+++ hazel/utils.py
@@ -1,7 +1,8 @@
 # coding=utf-8
-
+from __future__ import with_statement
 import os
 import codecs
+import shutil
 
 
 class ObjectDict(dict):
@@ -25,8 +26,26 @@ def render_template(template, **content):
     return template.render(content)
 
 
-def write(directory, filename, content):
-    with codecs.open(path_to_file(directory, filename + '.html'), mode='w', encoding='utf-8') as f:
+def safe_mkdir(directory):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+
+def force_mkdir(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+        os.mkdir(directory)
+    else:
+        os.mkdir(directory)
+
+
+def create(file_path, content):
+    with codecs.open(file_path, mode='w', encoding='utf-8') as f:
         f.write(content)
+
+
+def write(directory, filename, content):
+    create(path_to_file(directory, filename), content)
+
 
 g = ObjectDict
