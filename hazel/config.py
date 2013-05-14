a934e9ece71dd664b0a47e414e41d54fc0a96e50
Shunfan Du <i@perry.asia>
2013-05-14 19:11:29 +0800

    copy default config.

--- hazel/config.py
+++ hazel/config.py
@@ -3,8 +3,11 @@ import os
 import shutil
 
 from clint.textui import puts, indent, colored
-from hazel.utils import safe_mkdir, force_mkdir, create, write
 
+from hazel.utils import safe_mkdir, force_mkdir, write, g
+
+
+g.hazel_path = os.path.abspath(os.path.dirname(__file__))
 
 def default_mkdir():
     safe_mkdir('posts')
@@ -13,9 +16,13 @@ def default_mkdir():
     with indent(2, quote='>'):
         puts('all directories were initiated successfully.')
 
+
+def default_config():
+    default_config_path = os.path.join(g.hazel_path, 'default', 'config.yml')
+    shutil.copyfile(default_config_path, os.path.join(os.getcwd(), 'config.yml'))
+
 def install_default_template():
-    hazel_path = os.path.abspath(os.path.dirname(__file__))
-    default_template_path = os.path.join(hazel_path, 'default_template', 'hazel')
+    default_template_path = os.path.join(g.hazel_path, 'default', 'hazel')
     try:
         shutil.copytree(default_template_path, os.path.join(os.getcwd(), 'templates', 'hazel'))
         with indent(2, quote='>'):
@@ -27,9 +34,8 @@ def init():
     try:
         puts('Start initiating...')
         default_mkdir()
+        default_config()
         install_default_template()
-        config = '''site_name: A blog powered by hazel\ntemplate: hazel\nindex_post: 3'''
-        create('config.yml', config)
         puts(colored.green('Initiating process is done.'))
     except:
         puts(colored.red('Initiate fail...'))
