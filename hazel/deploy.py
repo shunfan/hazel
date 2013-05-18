0a06c443ba971814d15b829d52fcebfaa87b6cc3
Shunfan Du <i@perry.asia>
2013-05-18 12:19:49 +0800

    Add rsync depoly.

---
+++ hazel/deploy.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+import os
+
+from clint.textui import puts, indent, colored
+
+from hazel.load import load_config
+from hazel.utils import g
+
+
+def rsync():
+    try:
+    	load_config()
+        cmd = "rsync -avz site {user}@{host}:{dest}".format(
+            user=g.config.user,
+            host=g.config.host,
+            dest=g.config.dest,
+        )
+        os.system(cmd)
+        puts(colored.green('Deploy is complete.'))
+    except:
+        puts(colored.red('rsync failed.'))
