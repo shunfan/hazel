19744799482aa435569bb9c139e0778d04f7a3f1
Shunfan Du <i@perry.asia>
2013-05-26 10:20:18 +0800

    remove coveralls.

--- test/general_test.py
+++ test/general_test.py
@@ -1,14 +1,12 @@
 #coding=utf-8
 import os
 
-from hazel.config import init
-from hazel.build import new_post, generate
 from hazel.utils import force_mkdir
 
 
 def test():
     force_mkdir('test_blog')
     os.chdir('test_blog')
-    init()
-    new_post('test_post')
-    generate()
+    os.system('hazel init')
+    os.system('hazel write test_post')
+    os.system('hazel generate')
