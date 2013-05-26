e63de7cbd91a8d5a87eb0887f37dfcc97f624403
Shunfan Du <i@perry.asia>
2013-05-26 10:09:07 +0800

    test bug

--- test/general_test.py
+++ test/general_test.py
@@ -1,12 +1,14 @@
 #coding=utf-8
 import os
 
+from hazel.config import init
+from hazel.build import new_post, generate
 from hazel.utils import force_mkdir
 
 
 def test():
     force_mkdir('test_blog')
     os.chdir('test_blog')
-    os.system('hazel init')
-    os.system('hazel write test_post')
-    os.system('hazel generate')
+    init()
+    new_post('test_post')
+    generate()
